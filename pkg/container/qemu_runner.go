c59c488654f797fcd6f084d55c0b64d7e7803ac4
Steve Beattie <steve.beattie@chainguard.dev>
2025-10-22 15:28:58 -0700

    qemu runner: default to build user by default for builds
    Default to running builds in the QEMU runner as the unprivileged
    `build` user. This can be overridden with the `run-as:` accounts
    setting.
    
    For the time being, we'll keep tests under qemu defaulting to the
    root user.
    
    Signed-off-by: Steve Beattie <steve.beattie@chainguard.dev>

--- pkg/container/qemu_runner.go
+++ pkg/container/qemu_runner.go
@@ -112,8 +112,8 @@ func (bw *qemu) Run(ctx context.Context, cfg *Config, envOverride map[string]str
 func (bw *qemu) Debug(ctx context.Context, cfg *Config, envOverride map[string]string, args ...string) error {
 	clog.FromContext(ctx).Debugf("running debug command: %v", args)
 
-	// default to root user, unless a different user is specified
-	user := "root"
+	// default to build user, unless a different user is specified
+	user := "build"
 	if cfg.RunAs != "" {
 		user = cfg.RunAs
 	}
@@ -942,8 +942,8 @@ func setupSSHClients(ctx context.Context, cfg *Config) error {
 		return err
 	}
 
-	// default to root user, unless a different user is specified
-	user := "root"
+	// default to build user, unless a different user is specified
+	user := "build"
 	if cfg.RunAs != "" {
 		user = cfg.RunAs
 	}
